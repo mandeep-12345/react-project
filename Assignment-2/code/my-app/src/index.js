724f1dcccdd68131215b0a782c9f527723869f62
mandeep-12345 <mandiip.dass1997@gmail.com>
2022-04-05 11:39:26 +0530

    test-2

--- Assignment-2/code/my-app/src/index.js
+++ Assignment-2/code/my-app/src/index.js
@@ -6,12 +6,12 @@ import reportWebVitals from './reportWebVitals';
 
 
 const employee = {
-  name: 'Tom',
-  location: 'New York',
-  bloodGroup: 'B+',
-  age: 28,
+  name: 'Mandeep das',
+  Location: 'Assam',
+  BloodGroup: 'B+',
+  Age: 24,
   profileImg: 'https://www.w3schools.com/howto/img_avatar.png'
-       }
+}
 
 ReactDOM.render(
   <React.StrictMode>
